418870af7e3524ff437c5c081d3efa6d653b851a
HailSaeed <54307479+HailSaeed@users.noreply.github.com>
2023-09-15 16:16:14 +0530

    backend modification

--- Server/index.js
+++ Server/index.js
@@ -1,47 +1,45 @@
-const express = require('express')
-const app = express()
-const cors =require('cors')
-const mongoose = require('mongoose')
-// const PORT = process.env.PORT || ;
-require('dotenv').config({ path: './dev.env' });
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config({ path: "./dev.env" });
 
-// app.use(cors({
-//     origin: 'https://interactive-data-visualization-dashboard-frontend.vercel.app',
-//     methods: ['GET'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-const UserModel=require('./models/Users')
+const UserModel = require("./models/Users");
 
-const password = process.env.PASSWORD
-// console.log("password:", password)
+const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect(`mongodb+srv://Hail:${password}@cluster0.ma16vla.mongodb.net/MERN_STACK_ASSIGNMENT?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((error) => {
-    console.error('MongoDB connection error:', error);
+mongoose.connect(MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+app.get("/user", async (req, res) => {
+  try {
+    console.log("Hello");
+    const Users = await UserModel.find();
+    res.status(200).json(Users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
+app.use("/", (req, res) => {
+  res.send("wecome to Home page");
+});
 
-app.get('/user', async (req, res) => {
-    try {
-        console.log('Hello')
-        const Users = await UserModel.find();
-        res.status(200).json(Users);
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        res.status(500).send('Internal server error');
-    }
-})
-
-app.listen(3001, () => {
-    console.log(`Server running on port 3001`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
